224dcb64137d6afec493242805407f618541225e
Fernando Gomes <fcgomes@inf.ufrgs.br>
2019-09-16 12:23:36 -0300

    github link

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
+const { Client } = require('pg');
 
 const app = express();
 const server = http.Server(app);
